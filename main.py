509fce2b69e0dc3e60254c4b0db3e5cf394b09b9
empdo <emil.essung@live.se>
2021-08-27 19:11:48 +0200

    add start of board and block class

--- main.py
+++ main.py
@@ -1,14 +1,85 @@
 import pyglet
+from pyglet import shapes
+from pyglet.libs.win32.constants import NULL
+batch = pyglet.graphics.Batch()
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(600, 600, "tetris")
+block_size = 32
 
-label = pyglet.text.Label('Hello world', font_name='Times New Roman', font_size=36, x=window.width//2, y=window.height//2, anchor_x='center', anchor_y='center')  
+S = [
+    [
+        ".....",
+        ".....",
+        "..00.",
+        ".01..",
+        "....."
+    ],
+    [
+        ".....",
+        ".0...",
+        ".01.",
+        "..0..",
+        ".....",
+    ],
+]
+
+I = [
+    [
+        "..0..",
+        "..0..",
+        "..1..",
+        "..0..",
+        "..0..",
+    ],
+    [
+        ".....",
+        ".....",
+        "00100",
+        ".....",
+        ".....",
+    ],
+
+]
+
+blocks = {
+    "S": {
+        "shapes": S,
+        "color": (55, 55, 255)
+    },
+    "I": {
+        "shapes": I, 
+        "color": (55, 55, 255)
+    }
+
+}
+
+class Block:
+    def __init__(self, block_type):
+        self.block_type = blocks[block_type]
+        self.rotation = 0
 
 
-@window.event 
-def on_draw():     
+class Board:
+    def __init__(self):
+        self.grid = [[(74, 74, 74) for x in range(10)] for x in range(16)] 
+        self.current_block = NULL;
+        self.create_bundle()
+    
+    def draw_board(self):
+        for i, _i in enumerate(self.grid):
+            for j, _j in enumerate(self.grid[i]):
+                square = shapes.Rectangle(block_size, block_size, (j*block_size), (i*block_size), color=(self.grid[i][j]), batch=batch)
+        batch.draw()
+
+    def create_bundle(self):
+        pass
+
+
+board = Board()
+
+@window.event
+def on_draw():
     window.clear()
-    label.draw()
+    board.draw_board()
 
 pyglet.app.run()
-
