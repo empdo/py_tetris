7135cd57b6f9d0238d33870704822a4685e34bc9
empdo <emil.essung@live.se>
2021-08-30 16:16:13 +0200

    added shapes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from shapes import *
 import pyglet
 import copy
 import random
@@ -15,21 +16,37 @@ window = pyglet.window.Window(600, 600, "tetris")
 block_size = 32
 default_grid = [[(255, 229, 158) for x in range(11)] for x in range(17)]
 placed_blocks = []
+drop_time = 0.7
 
-#shapes
-from shapes import *
+# shapes
 
-#blocks
+# blocks
 blocks = {
-    "S": {
-        "shapes": S,
-        "color": (245, 46, 32)
-    },
     "I": {
         "shapes": I,
         "color": (55, 55, 255)
-    }
-
+    },
+    "S": {
+        "shapes": S,
+        "color": (114, 203, 59)
+    },
+    "L":
+        {
+            "shapes": L,
+            "color": (255, 151, 28)
+    },
+    "J": {
+        "shapes": J,
+        "color":  (3, 65, 174)
+    },
+    "Z": {
+        "shapes": Z,
+        "color": (114, 203, 59)
+    },
+    "T": {
+        "shapes": T,
+        "color": (3, 65, 174)
+    },
 }
 
 
@@ -40,10 +57,10 @@ class Block:
         self.color = color
         self.position = self.default_pos()
 
-    #setts default position of block
+    # setts default position of block
     def default_pos(self):
         _position = []
-        
+
         for i, _i in enumerate(self.block_type["shapes"][0]):
             for j, _j in enumerate(_i):
                 if _j != ".":
@@ -55,42 +72,55 @@ class Block:
         _position = []
         _rotation = self.rotation
 
-        #get rotation point
+        # get rotation point
         for position in self.position:
             if "1" in position:
                 rotation_point = position
 
-        #clear positions and add one to the rotation
+        # clear positions and add one to the rotation
         _position.append(rotation_point)
         _rotation = (self.rotation + 1) % len(self.block_type["shapes"])
 
-        #set varje block till diffrensen i position från rotation point + rotaion point eller något jag behöver mer hjärnceller
-        for i, _i in enumerate(self.block_type["shapes"][self.rotation]):
+        # set varje block till diffrensen i position från rotation point + rotaion point eller något jag behöver mer hjärnceller
+        for i, _i in enumerate(self.block_type["shapes"][_rotation]):
             for j, _j in enumerate(_i):
                 if _j != "." and _j != "1":
-                    _position.append([rotation_point[0] - (2 - i), rotation_point[1] - (2 - j), _j])
+                    _position.append(
+                        [rotation_point[0] - (2 - i), rotation_point[1] - (2 - j), _j])
 
-        if self.can_rotate(_position):
+        can_rotate = self.can_rotate(_position, _rotation)
+        if can_rotate[0] or can_rotate[1] != 0:
+            for i in _position:
+                i[1] += can_rotate[1]
             self.position = _position
+            self.rotation = _rotation
 
+    def can_rotate(self, position, rotation_point):
+        blocks_outside = 0
+        blocks_in_blocks = 0
 
-    def can_rotate(self, position):
-        outside_diff = None
-
-        #Returna false om de inte fungerar men med en diff på om blocket
-        #kan och behöver flytta sig åt sidan om den är utanför eller i ett annat block
+        # Returna false om de inte fungerar men med en diff på om blocket \
+        # kan och behöver flytta sig åt sidan om den är utanför eller i ett annat block
 
         for i in position:
             if (i[1] < 0):
-               if (outside_diff == None or i[1] < outside_diff): 
-                   outside_diff = i[1]
+                blocks_outside += 1
             elif (i[1] > 10):
-                if (outside_diff == None or i[1] > outside_diff):
-                    outside_diff = i[1]
-               
+                blocks_outside -= 1
 
-#TODO: fixa småfel
-#TODO: veta om det är bra eller dåligt med tre miljarder funktioner
+        for i in position:
+            for j in placed_blocks:
+                if ((i[0], i[1] + blocks_outside) == (j[0], j[1])):
+                    return(False, 0)
+
+        if blocks_outside != 0:
+            return([False, blocks_outside])
+        else:
+            return([True, 0])
+
+
+# TODO: fixa småfel
+# TODO: veta om det är bra eller dåligt med tre miljarder funktioner
 
 class Board:
     def __init__(self):
@@ -101,8 +131,8 @@ class Board:
         self.queue = []
         self.spawn_block()
 
+    # draws the board from grid 🤯
 
-    #draws the board from grid 🤯
     def draw_board(self):
         for i, _i in enumerate(self.grid):
             for j, _j in enumerate(self.grid[i]):
@@ -111,10 +141,10 @@ class Board:
 
         batch.draw()
 
-    #makes a blank board, sets the current block then the placed blocks
+    # makes a blank board, sets the current block then the placed blocks
     def update_board(self):
         if self.current_block:
-            self.grid = copy.deepcopy(default_grid) #TODO: borde vara default_grid #TODO: borde vara default_grid
+            self.grid = copy.deepcopy(default_grid)
             for i in self.current_block.position:
                 self.grid[i[0]][i[1]] = self.current_block.color
             for i in placed_blocks:
@@ -122,7 +152,7 @@ class Board:
 
         self.draw_board()
 
-    #spawn block, will probably do more
+    # spawn block, will probably do more
     def spawn_block(self):
         if (len(self.queue) <= 1):
             self.create_bundle()
@@ -130,7 +160,7 @@ class Board:
         self.current_block = Block(self.queue[0][0], self.queue[0][1])
         self.queue.pop(0)
 
-        self.update_board()        
+        self.update_board()
 
     def block_down(self):
         # if new position not in placed_blocks
@@ -140,18 +170,17 @@ class Board:
                 i[0] -= 1
         else:
             for i in positions:
-                placed_blocks.append([i[0],i[1], self.current_block.color])
+                placed_blocks.append([i[0], i[1], self.current_block.color])
             self.spawn_block()
 
-        self.update_board() 
+        self.update_board()
 
-    #TODO: change name
+    # TODO: change name
     def block_side(self, change):
         positions = self.current_block.position
         if self.can_move(0, change):
             for i in positions:
                 i[1] += change
-
 
     def piece_rotate(self):
         self.current_block.rotate_pos()
@@ -172,32 +201,34 @@ class Board:
     def can_rotate():
         pass
 
-    #creates a bundle then adds it to queue
+    # creates a bundle then adds it to queue
     def create_bundle(self):
-        #scramble _blocks
+        # scramble _blocks
         for i in blocks:
-           self.queue.append([str(i), blocks[i]["color"]])
-
+            self.queue.append([str(i), blocks[i]["color"]])
 
     def rotate():
         pass
 
 
 board = Board()
+
+
 @window.event
 def on_key_press(symbol, modifiers):
-  if symbol == key.LEFT:
-    board.block_side(-1)
-  elif symbol == key.RIGHT:
-    board.block_side(1)
-  elif symbol == key.UP:
-    board.piece_rotate()
+    if symbol == key.LEFT:
+        board.block_side(-1)
+    elif symbol == key.RIGHT:
+        board.block_side(1)
+    elif symbol == key.UP:
+        board.piece_rotate()
 
 
 @window.event
 def on_draw():
     window.clear()
     board.update_board()
+
 
 def update_frames(var):
     if board.current_block != None:
@@ -207,6 +238,6 @@ def update_frames(var):
         board.create_bundle()
 
 
-pyglet.clock.schedule_interval(update_frames, 0.7)
+pyglet.clock.schedule_interval(update_frames, drop_time)
 
 pyglet.app.run()
